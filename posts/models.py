a292ae670a3800ef7de4665e2c6f9bac6121ea53
Faith Chepngeno <faithchepngeno850@gmail.com>
2025-11-15 01:53:06 +0300

    inital commit

---
+++ posts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    text=models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.text[:50]  # Return the first 50 characters of the post text
